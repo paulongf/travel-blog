06f40bb7e98cdf07157502428097aeec3d2a06df
Paulongf <paulongf@hotmail.com>
2025-05-07 14:58:52 +0100

    Alterando Url para o deploy

--- frontend/src/redux/features/comments/commentApi.js
+++ frontend/src/redux/features/comments/commentApi.js
@@ -1,29 +1,38 @@
-import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+// Função para determinar a URL base com base no ambiente
+const getBaseUrl = () => {
+  if (window.location.hostname === "localhost") {
+    return "http://localhost:5000/api/comments";
+  } else {
+    return "https://reviewspopcorn-czbqgcb0g2cucpb0.westeurope-01.azurewebsites.net/api/comments"; // substitua com sua URL real do Azure
+  }
+};
 
 const commentApi = createApi({
-    reducerPath: "commentApi",
-    baseQuery: fetchBaseQuery({
-        baseUrl: "http://localhost:5000/api/comments",
-        credentials: "include"
+  reducerPath: "commentApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: getBaseUrl(),
+    credentials: "include",
+  }),
+  tagTypes: ['Comments'],
+  endpoints: (builder) => ({
+    postComment: builder.mutation({
+      query: (commentData) => ({
+        url: "/post-comment",
+        method: "POST",
+        body: commentData,
+      }),
+      invalidatesTags: (result, error, { postId }) => [{ type: 'Comments', id: postId }],
     }),
-    tagTypes: ['Comments'], 
-    endpoints: (builder) => ({
-        postComment: builder.mutation({
-            query: (commentData) => ({
-                url: "/post-comment",
-                method: "POST",
-                body: commentData
-            }),
-            invalidatesTags: (result, error, {postId}) => [{type: 'Comments', id: postId}]
-        }),
-        getComments: builder.query({
-            query: () => ({
-                url: "/total-comments",
-                method: "GET",
-            }),
-        })
-    })
-})
+    getComments: builder.query({
+      query: () => ({
+        url: "/total-comments",
+        method: "GET",
+      }),
+    }),
+  }),
+});
 
-export const {useGetCommentsQuery, usePostCommentMutation} = commentApi;
+export const { useGetCommentsQuery, usePostCommentMutation } = commentApi;
 export default commentApi;
